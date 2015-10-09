bf3ecf5faed2dd04f74697cf7888e96d972af4d0
rsbarhey <barheyan@yahoo.com>
2015-10-09 06:01:59 -0600

    Added Documentation in UrlParser

--- TCPClient/src/UrlParser.java
+++ TCPClient/src/UrlParser.java
@@ -1,11 +1,21 @@
 
-
+/**
+ * 
+ * @author Ragheb Barheyan
+ * @version 1.3, Oct 9, 2015
+ */
 public class UrlParser
 {
 	private String hostName = "";
 	private String path = "";
 	private int port = 80;
-	
+
+	/**
+	 * This is constructor for UrlParser and it parses the passed string into
+	 * Host Name, Path, and Port Number 
+	 * @param url A string to be parsed
+	 * @throws InvalidUrlException If url is empty or the port string cannot be converted into a number
+	 */
 	public UrlParser(String url) throws InvalidUrlException
 	{
 		if(url.isEmpty())
@@ -33,16 +43,28 @@ public class UrlParser
 		}
 	}
 	
+	/**
+	 * 
+	 * @return the Host Name parsed by the constructor 
+	 */
 	public String GetHostName()
 	{
 		return hostName;
 	}
 	
+	/**
+	 * 
+	 * @return the Path parsed by the constructor
+	 */
 	public String GetPath()
 	{
 		return path;
 	}
 	
+	/**
+	 * 
+	 * @return the Port Number parsed by the constructor as int
+	 */
 	public int GetPort()
 	{
 		return port;
