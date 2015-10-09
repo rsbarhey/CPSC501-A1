b18ec9065421dea3163a5370c6f0ea42e9a3ecf5
rsbarhey <barheyan@yahoo.com>
2015-10-09 07:10:42 -0600

    Copied the methods from UrlCache that are needed in ResponseReader Created test cases for content length status code, and last modified, found a bug through testing last modified and now it's fixed

--- TCPClient/src/ResponseReader.java
+++ TCPClient/src/ResponseReader.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.io.InputStream;
 
 /**
@@ -12,10 +13,25 @@ public class ResponseReader
 	private int statusCode;
 	private int contentLength;
 	private String lastModified;
+	byte[] responseBody;
+	byte[] bytesToWrite;
 	
 	public ResponseReader(InputStream in)
 	{
-		
+		responseBody = new byte[10 * 2048];
+		try {
+			in.read(responseBody);
+			dataIndex = setDataIndex(responseBody);
+			contentLength = setContentLength(responseBody);
+			statusCode = setStatusCode(responseBody);
+			lastModified = setLastModified(responseBody);
+			
+			bytesToWrite = new byte[contentLength];
+			setBytesToWrite();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	/**
@@ -51,6 +67,129 @@ public class ResponseReader
 	 */
 	public String GetLastModified()
 	{
+		return lastModified;
+	}
+	
+	public byte[] GetBytesToWrite()
+	{
+		return bytesToWrite;
+	}
+	
+	private int setDataIndex(byte[] bytes)
+	{
+		int i = 0;
+		while(i<bytes.length && i+1 < bytes.length &&
+				i+2 <bytes.length && i + 3 <bytes.length)
+		{
+			if(Character.toChars(bytes[i])[0]== '\r' && 
+					Character.toChars(bytes[i+1])[0] == '\n' &&
+						Character.toChars(bytes[i+2])[0]== '\r' && 
+							Character.toChars(bytes[i+3])[0] == '\n')
+			{
+				break;
+			}
+			i++;
+		}
+		return i + 4;
+	}
+	
+	private int setContentLength(byte[] bytes)
+	{
+		int contentLength = 0;
+		String tmpHeader = "";
+		for(int i = 0; i < bytes.length; i++)
+		{
+			if(Character.toChars(bytes[i])[0] != '\n')
+			{
+				tmpHeader += Character.toChars(bytes[i])[0];
+			}
+			if(Character.toChars(bytes[i])[0] == '\r')
+			{
+				if(tmpHeader.startsWith("Content-Length"))
+				{
+					String[] parsedHeader = tmpHeader.split(" ");
+					if(parsedHeader.length > 1)
+					{
+						String number = parsedHeader[1].replace("\r", "");	// remove the \r from parsed header
+						contentLength = Integer.parseInt(number);
+						break;
+					}
+				}
+				else
+				{
+					tmpHeader = "";
+				}
+			}
+		}
+		return contentLength;
+	}
+	
+	private int setStatusCode(byte[] bytes)
+	{
+		int statusCode = 0;
+		String tmpHeader = "";
+		for(int i = 0; i < bytes.length; i++)
+		{
+			if(Character.toChars(bytes[i])[0] != '\n')
+			{
+				tmpHeader += Character.toChars(bytes[i])[0];
+			}
+			if(Character.toChars(bytes[i])[0] == '\r')
+			{
+				if(tmpHeader.startsWith("HTTP"))
+				{
+					String[] parsedHeader = tmpHeader.split(" ");
+					if(parsedHeader.length > 1)
+					{
+						String number = parsedHeader[1].replace("\r", "");	// remove the \r from parsed header
+						statusCode = Integer.parseInt(number);
+						break;
+					}
+				}
+				else
+				{
+					tmpHeader = "";
+				}
+			}
+		}
+		return statusCode;
+	}
+	
+	private void setBytesToWrite()
+	{
+		for(int i = 0; i < contentLength; i++)
+		{
+			bytesToWrite[i] = responseBody[i+dataIndex];
+		}
+	}
+	
+	private String setLastModified(byte[] bytes)
+	{
+		String lastModified = "";
+		String tmpHeader = "";
+		for(int i = 0; i < bytes.length; i++)
+		{
+			if(Character.toChars(bytes[i])[0] != '\n')
+			{
+				tmpHeader += Character.toChars(bytes[i])[0];
+			}
+			if(Character.toChars(bytes[i])[0] == '\r')
+			{
+				if(tmpHeader.startsWith("Last-Modified"))
+				{
+					String[] parsedHeader = tmpHeader.split(" ", 2);
+					if(parsedHeader.length > 1)
+					{
+						lastModified = parsedHeader[1].replace("\r", "");	// remove the \r\n from parsed header
+						break;
+					}
+				}
+				else
+				{
+					tmpHeader = "";
+				}
+			}
+		}
 		return lastModified;
 	}
 }
--- TCPClient/src/TestResponseReader.java
+++ TCPClient/src/TestResponseReader.java
@@ -3,21 +3,68 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-public class TestResponseReader {
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
 
+public class TestResponseReader {
+	
 	@Test
 	public void testHttp200() {
-		fail("Not yet implemented");
+		try {
+			String httpReq = "GET /~mghaderi/test/test.html HTTP/1.1 \r\n"
+					+ "Host: people.ucalgary.ca:80 \r\n";
+			Socket socket = new Socket("people.ucalgary.ca", 80);
+			PrintWriter out = new PrintWriter(new DataOutputStream(socket.getOutputStream()));
+			out.println(httpReq+"\r\n");
+			out.flush();
+			ResponseReader responseReader = new ResponseReader(socket.getInputStream());
+			
+			assertEquals(200, responseReader.GetStatusCode());
+			socket.shutdownOutput();
+			socket.shutdownInput();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Test
 	public void testHttp304()
 	{
-		fail("Not yet implemented");
+		try {
+			Socket socket = new Socket("static.ucalgary.ca", 80);
+			PrintWriter out = new PrintWriter(new DataOutputStream(socket.getOutputStream()));
+			out.println("GET /2013-001/980/global/images/identity/vertical-crest.png HTTP/1.1\r\n"
+					+ "host: static.ucalgary.ca\r\n"
+					+ "If-Modified-Since: Fri, 02 Oct 2015 20:00:00 GMT\r\n\r\n");
+			out.flush();
+			ResponseReader responseReader = new ResponseReader(socket.getInputStream());
+			
+			assertEquals(304, responseReader.GetStatusCode());
+			socket.shutdownOutput();
+			socket.shutdownInput();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
+	@Test
 	public void testLastModified()
 	{
-		fail("Not yet implemented");
+		try {
+			Socket socket = new Socket("static.ucalgary.ca", 80);
+			PrintWriter out = new PrintWriter(new DataOutputStream(socket.getOutputStream()));
+			out.println("GET /2013-001/980/global/images/identity/vertical-crest.png HTTP/1.1\r\n"
+					+ "host: static.ucalgary.ca\r\n\r\n");
+			out.flush();
+			ResponseReader responseReader = new ResponseReader(socket.getInputStream());
+			
+			assertEquals("Fri, 30 Aug 2013 18:43:48 GMT", responseReader.GetLastModified());
+			socket.shutdownOutput();
+			socket.shutdownInput();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
